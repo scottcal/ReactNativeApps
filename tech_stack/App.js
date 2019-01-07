6d230f4000ed948cf2b00f505131ffa540ff6c33
Scott <scottcal@gmail.com>
2019-01-06 20:08:44 -0800

    Redux Libary Reducer

--- tech_stack/App.js
+++ tech_stack/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header,Spinner } from './src/components/common';
+import LibraryList from './src/components/LibraryList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View />
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header headerText="Tech Stack"/>
+          <LibraryList />
+        </View>
+      </Provider>
     );
   }
 }
---
+++ tech_stack/src/components/common/index.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-unresolved
+export * from './Button';
+// eslint-disable-next-line import/no-unresolved
+export * from './Card';
+// eslint-disable-next-line import/no-unresolved
+export * from './CardSection';
+// eslint-disable-next-line import/no-unresolved
+export * from './Header';
+// eslint-disable-next-line import/no-unresolved
+export * from './Input';
+export * from './Spinner';
