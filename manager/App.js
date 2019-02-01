c2541b9ff2e84f3470c3a44e5d029be3a2d81343
Scott Calafiore <scottcal@gmail.com>
2019-01-31 16:12:04 -0800

    Update to Login Form

--- manager/App.js
+++ manager/App.js
@@ -2,38 +2,33 @@ import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase'; 
 import reducers from './src/reducers';
+import LoginForm from './src/components/LoginForm';
 
 export default class App extends Component {
+
+  componentWillMount() {
+      const config = {
+      apiKey: 'AIzaSyDLeCeR2bc_SUJ5bIM5y19ApJyz1rJu_Sc',
+      authDomain: 'manager-ced17.firebaseapp.com',
+      databaseURL: 'https://manager-ced17.firebaseio.com',
+      projectId: 'manager-ced17',
+      storageBucket: 'manager-ced17.appspot.com',
+      messagingSenderId: '174152235896'
+    };
+    firebase.initializeApp(config);
+  
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
       <View>
-        <Text>
-          Hello!
-        </Text>
+        <LoginForm />
       </View>
       </Provider>
     );
   }
 };
 
-/*
-import React, {Component} from 'react';
-import { Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-export default class App extends Component {
-  render() {
-    return (
-    <Provider store = {createStore()}>
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-      </Provider>
-    );
-  }
-};
-
-*/
