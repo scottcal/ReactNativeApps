81868ec53445ebf6ce1662789cf702bc3f1c4ded
Scott Calafiore <scottcal@gmail.com>
2019-02-13 16:10:30 -0800

    Update Login Form / Reducers

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -5,7 +5,7 @@ import {Card, CardSection, Input, Button} from './common'
 
 class LoginForm extends Component {
     onEmailChange (text) {
-
+        this.props.emailChanged(text);
     }
     
     
@@ -17,6 +17,7 @@ class LoginForm extends Component {
                         label="Email"
                         placeholder="email@gmail.com"
                         onChangeText={this.onEmailChange.bind(this)}
+                        value={this.props.email}
                     />
                 </CardSection>
                 <CardSection>
@@ -38,4 +39,10 @@ class LoginForm extends Component {
     }
 }
 
-export default connect(null, {emailChanged}) (LoginForm);
+const mapStateToProps = state => {
+    return {
+        email: state.auth.email
+    };
+};
+ 
+export default connect(mapStateToProps, {emailChanged}) (LoginForm);
