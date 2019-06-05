eff7b825b95fb3dea28dcf7f54ad1cbcc17fbc96
Scott Calafiore <scottcal@gmail.com>
2019-06-04 22:00:39 -0700

    add set state to axios response

--- albums2/src/components/AlbumList.js
+++ albums2/src/components/AlbumList.js
@@ -3,12 +3,18 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [],x: 1 };
+
     componentDidMount() {
             axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-                .then(response => console.log(response.data));
+                .then(response => this.setState({ albums: response.data }));
+            this.setState({ x: this.state.x + 1 });
         }
 
     render() {
+        let x = x + 1;
+        console.log(this.state.albums);
+        console.log(this.state.x);
         return (
             <View>
                 <Text>AlbumList</Text>
