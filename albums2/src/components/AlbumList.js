3f0fa67d4ce5fe13899613062fc9c89d9f673cda
Scott Calafiore <scottcal@gmail.com>
2019-06-08 14:20:01 -0700

    Render Albums
    add render method to show albums

--- albums2/src/components/AlbumList.js
+++ albums2/src/components/AlbumList.js
@@ -3,21 +3,31 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
-    state = { albums: [],x: 1 };
+    state = { albums: [], x: 1 };
+    //constructor() {
+    //    var x = 0;
+    //}
+    
 
     componentDidMount() {
+            let myvar;
             axios.get('https://rallycoding.herokuapp.com/api/music_albums')
                 .then(response => this.setState({ albums: response.data }));
             this.setState({ x: this.state.x + 1 });
         }
 
+    renderAlbums() {
+        return this.state.albums.map(album => <Text>{album.title}</Text>);
+    } 
+
     render() {
-        let x = x + 1;
+        
         console.log(this.state.albums);
         console.log(this.state.x);
+        //console.log('x is: ',x);
         return (
             <View>
-                <Text>AlbumList</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
